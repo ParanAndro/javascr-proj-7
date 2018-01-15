d368a682d5cff9579416939fb26149a02c08c59b
Radek Izdebski <radek1izdebski@gmail.com>
2018-01-15 18:20:05 +0100

    Add scripts.js

---
+++ scripts.js
@@ -0,0 +1,13 @@
+//scripts.js
+function drawTree (a) {
+	for (var i = 0; i < a; i++) {
+		var star = " ";
+		for (var j = 0; j <= i; j++) {
+			star += "*";
+		} 
+		console.log(star);
+	}
+}
+
+drawTree (7)
+
